85c5e6fcde33e35caae4eb374dee846cf64dfbc3
anu-xo <anuradhaa.vits@gmail.com>
2026-08-08 18:26:08 +0530

    count number of digits

--- countnumbers.cpp
+++ countnumbers.cpp
@@ -1,13 +1,26 @@
+// 
+
+
 #include <iostream>
+#include <vector>
+#include <algorithm>
 using namespace std;
+
+int countdigits(int n){
+    int count = 0;
+    while (n>0){
+        n = n /10;
+        count++;
+    }
+    return count;
+}
 
 int main(){
     int num;
-    int count;
-    cout << "Enter a number: ";
+    cout << "heyy!! Today i am going to count the digits of a number" << endl;
+    cout << "Please enter a number: ";
     cin >> num;
-    cout << "The number you entered is " << num << endl;
-    count =to_string(num).length();
-    cout << "The number of digits are: " << count << endl;
+    int digits = countdigits(num);
+    cout << "The number of digits in " << num << "is: " << digits << endl;
     return 0;
-}    
+}
